a41511e1310468a93d76221357af803c5f54bfe5
hox <eric.hoxworth@gmail.com>
2012-12-07 13:28:28 -0600

    Initial port from imonggo to vendhq.

---
+++ VendHQ/api/resources/Documents.py
@@ -0,0 +1,15 @@
+from VendHQ.api.lib.mapping import Mapping
+from VendHQ.api.lib.filters import FilterSet, StringFilter, NumberFilter, DateFilter, BoolFilter
+from . import ResourceObject
+
+
+class StockMovements(ResourceObject):
+    
+    @classmethod
+    def filter_set(cls):
+        fs = FilterSet(before = DateFilter(),
+                         after = DateFilter(),
+                         to = DateFilter(),
+                         ) 
+        fs["from"] = DateFilter()
+        return fs
